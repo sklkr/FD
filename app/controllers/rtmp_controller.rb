2542008fb4a50c7ea39724598026e423437acee4
sidongithub <koolspy.siddhu@gmail.com>
2015-05-22 09:25:23 +0530

    unknown

--- app/controllers/rtmp_controller.rb
+++ app/controllers/rtmp_controller.rb
@@ -1,4 +1,20 @@
 class RtmpController < ApplicationController
   def index
+   parsed_uri = URI.parse('http://dkv8md9go69g2.cloudfront.net') 
+   url = "#{parsed_uri.scheme}://#{parsed_uri.host}#{parsed_uri.path}/nghia.mp4"   
+   @video_path = sign(url)
+   @flash_path = signed_flash
+  end
+
+  def signed_flash
+   rtmp_url = "rtmp://s2kj5stk8cuu5x.cloudfront.net"
+   rtmp_path = sign("/nghia.mp4")
+   full_url = "#{rtmp_url}#{rtmp_path}"
+  end  
+
+
+  def sign(url="")
+    # 1 hour expiration on the URL
+    url = SIGNER.sign(url.to_s, :ending => Time.now + 100)
   end
 end
---
+++ config/initializers/aws_signing.rb
@@ -0,0 +1,4 @@
+# Add config/keys/pk-someRandomString.pem to your .gitignore file so...
+# that it isn't stored in source code repos
+ 
+SIGNER = AwsCfSigner.new('/home/sid/sites/Fitness/config/keys/pk-APKAJ6OC2JJSEKTI3RGA.pem')
