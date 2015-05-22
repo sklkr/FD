040207e9642c3cbaacb1f753e52465ca0bdaaa5d
sidongithub <koolspy.siddhu@gmail.com>
2015-05-22 13:38:07 +0530

    settings for refile changed for new version of s3

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,11 +1,12 @@
-require "refile/backend/s3"
+require "refile/s3"
 
  Refile.host = "http://d1djl7kr085bc1.cloudfront.net/"
 
 aws = {
  access_key_id: "AKIAJLFLP6QXQ5WP5CZQ",
  secret_access_key: "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",
+ region: "Singapore",
  bucket: "fitnesspapav3",
 }
 
-Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
+Refile.store = Refile::S3.new(prefix: "store", **aws)
