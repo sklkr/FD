844e0afe2a8662884c707c72d3028eecdd6a0e36
sidongithub <koolspy.siddhu@gmail.com>
2015-05-22 17:19:10 +0530

    partners signup

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,6 +1,7 @@
 class PartnersController < ApplicationController
 layout 'homepage'
   def index
+    
   end
 
   def new
@@ -13,7 +14,7 @@ layout 'homepage'
   	@partner = Partner.new(permit_params)
   	if @partner.save
   		AcknowledgeMailer.partner_reg(@partner).deliver
-  		redirect_to root_url, :notice => "Thank you for showing interest as a FitnessPapa parter. We'll get back to you soon"
+  		redirect_to new_partner_path, :notice => "Thank you for showing interest as a FitnessPapa parter. We'll get back to you soon"
   	else
   		render 'new'
   	end
