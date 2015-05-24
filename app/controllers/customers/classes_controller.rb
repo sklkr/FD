2f259f8418e9ed993d4697f2e0e0178ca9858eca
sidongithub <koolspy.siddhu@gmail.com>
2015-05-24 18:47:21 +0530

    remaining tickets showing at custooemr

--- app/controllers/customers/classes_controller.rb
+++ app/controllers/customers/classes_controller.rb
@@ -3,6 +3,7 @@ class ClassesController < ApplicationController
 layout 'customerdashboard'
   def index
   	@passport = passport
+  	@total = passport.try(:remaining_tickets) || 0 
   	@bookings = @passport.clasbkings.includes(:fpclass) unless @passport.nil?
   	@history = Clasbking.unscoped.where('customer_id=?', current_user.customer.id)
   end
