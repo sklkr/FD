2df7bf7766ee64f92dae80e3df643f24f0e44286
sidongithub <koolspy.siddhu@gmail.com>
2015-05-30 17:13:49 +0530

    Mandrill production url configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,11 +57,11 @@ Rails.application.configure do
   # config.action_controller.asset_host = "d2yvb3j5b4tgn0.cloudfront.net"
   config.action_controller.asset_host = "d1djl7kr085bc1.cloudfront.net"
 
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "fitnesspapa.com" }
+  config.mandrill_mailer.default_url_options = { :host => "fitnesspapa.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
