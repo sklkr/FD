d8caac74a9e1b3ba2c5254b5474b943f28407782
sidongithub <koolspy.siddhu@gmail.com>
2015-05-30 21:52:08 +0530

    sms format changed

--- app/models/clasbking.rb
+++ app/models/clasbking.rb
@@ -14,11 +14,11 @@ class Clasbking < ActiveRecord::Base
   end
 
   def tell_partner
-    SmsService.new(phone_number, "#{self.customer.user.full_name} has registered #{self.fpclass.name} on #{self.expired_at}.   FitnessPapa").delay.send_sms
+    SmsService.new(phone_number, "#{self.customer.user.full_name} has registered #{self.fpclass.name} on #{self.expired_at} at #{self.fpclass.start_time.strftime('%H:%M')}.").delay.send_sms
 	end
 
   def tell_customer
-    SmsService.new(customer.user.phone, "Registration for #{self.fpclass.name} at #{self.center.name} on date #{self.expired_at} is confirmed successfully.   Thank you").delay.send_sms
+    SmsService.new(customer.user.phone, "Registration for #{self.fpclass.name} at #{self.center.name} on #{self.expired_at} at #{self.fpclass.start_time.strftime('%H:%M')} is confirmed successfully.").delay.send_sms
   end
 
 
