b57cf33d736ef2e88486b4eaf27992a7294235f7
sidongithub <koolspy.siddhu@gmail.com>
2015-06-05 10:39:08 +0530

    reservations mistake v2

--- app/controllers/partners/reservations_controller.rb
+++ app/controllers/partners/reservations_controller.rb
@@ -6,7 +6,9 @@ layout 'partners'
     @fpclasses = current_partner.fpclasses.includes(:clasbkings)
     @fpclasses.each {|fpclass| clasbkings << fpclass.clasbkings }
     @clasbkings = clasbkings.flatten
-    @past = current_partner.clasbkings.unscoped.where('expired_at < ?', Time.now)
+    past = []
+    @pastfpclasses = Fpclass.unscoped.where('partner_id=?',26).pluck(:id)
+    @past = Clasbking.unscoped.where('expired_at < ?', Time.now).where(:fpclass_id=>@pastfpclasses)
   end
 end
 end
