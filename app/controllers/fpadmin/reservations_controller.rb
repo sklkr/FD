5bb94ea8c2a8e2897802cc60f0430a82d36e2f1d
sidongithub <koolspy.siddhu@gmail.com>
2015-06-16 19:16:59 +0530

    reservations history

--- app/controllers/fpadmin/reservations_controller.rb
+++ app/controllers/fpadmin/reservations_controller.rb
@@ -5,6 +5,7 @@ before_filter :authenticated?
 
   def index
     @reservations = Clasbking.recent.includes(:customer, :fpclass, :center).page params[:page]
+    @history = Clasbking.unscoped.where('expired_at < ?', Time.now).includes(:customer, :fpclass, :center).page params[:page]
   end
 
   def show
