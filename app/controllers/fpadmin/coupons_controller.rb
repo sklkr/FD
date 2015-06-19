654fc02c59dca70d98d44e4afdc87aa41c08518c
sidongithub <koolspy.siddhu@gmail.com>
2015-06-19 19:01:41 +0530

    working on payumoney dynamic hash generator

--- app/controllers/fpadmin/coupons_controller.rb
+++ app/controllers/fpadmin/coupons_controller.rb
@@ -4,13 +4,28 @@ layout 'fpadmin'
 before_filter :authenticated?
 
   def index
-    binding.pry
+   @coupons = Coupon.all
   end
 
-  def show
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.new(permit_params)
+    if @coupon.save
+      redirect_to action: :index, :notice => "Coupon Added"
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
+
+  private
+    def permit_params
+      params.require(:coupon).permit(:code, :expires_at, :percent, :amount)
+    end
 end
 end
